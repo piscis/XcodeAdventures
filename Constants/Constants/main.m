a16a9a10a66f774daeb66a8836aed46d8513c363
Alexander Pirsig <apirsig@web.de>
2012-08-30 18:48:05 +0200

    adding constants

--- Constants/Constants/main.m
+++ Constants/Constants/main.m
@@ -8,6 +8,11 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum  {
+    MODE_TWO,
+    MODE_ONE
+} BlenderSpeed;
+
 int main(int argc, const char * argv[])
 {
 
@@ -15,6 +20,11 @@ int main(int argc, const char * argv[])
         
         // In literal NSString, use \u for arbitrary unicode chars
         NSLog(@"\u03c0 is %f", M_PI);
+        NSLog(@"%d is larger", MAX(10,12));
+        
+        NSLocale *here = [NSLocale currentLocale];
+        NSString *currency = [here objectForKey:NSLocaleCurrencyCode];
+        NSLog(@"Money is %@",currency);
         
     }
     return 0;
